1747127824caf34173cab6b8e4b17db1499b6c8d
liushili <996967616@qq.com>
2017-05-03 23:58:25 +0800

    FIX 修改产品单结构

--- produce_order/admin.py
+++ produce_order/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ProductSize, Customer, ProduceOrder
+from .models import ProductSize, Company, ProduceOrder, Product
 
 
 @admin.register(ProductSize)
@@ -10,35 +10,32 @@ class ProductSizeAdmin(admin.ModelAdmin):
     list_display = ('kind', 'value')
 
 
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'person', 'phone', 'fax', 'kind')
 
 
-@admin.register(ProduceOrder)
-class ProduceOrderAdmin(admin.ModelAdmin):
-    list_display = ('order_number', 'name', 'style',
-                    'customer_name', 'display_size',
-                    'box_number', 'pay_time', 'create_time')
-    list_select_related = ('customer', 'length', 'width', 'height')
-    fieldsets = (
-        (None, {
-            'fields': ('order_number', 'name', 'style')
-        }),
-        (u'产品尺寸', {
-            'fields': ('length', 'width', 'height')
-        }),
-        (u'订单交付', {
-            'fields': ('customer', 'box_number', 'pay_time')
-        })
-    )
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'style', 'company_name', 'length', 'width', 'height')
 
-    def customer_name(self, obj):
-        return obj.customer.name
+    def company_name(self, obj):
+        return obj.company.name
 
     def display_size(self, obj):
         return '长: (%s)  宽: (%s)  高: (%s)' % (obj.length.value, obj.width.value, obj.height.value)
 
-    customer_name.short_description = u'客户名称'
+    company_name.short_description = u'客户名称'
     display_size.short_description = u'产品尺寸'
+
+
+@admin.register(ProduceOrder)
+class ProduceOrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'company_name', 'pay_time', 'create_time')
+    list_select_related = ('company',)
+
+    def company_name(self, obj):
+        return obj.company.name
+
+    company_name.short_description = u'客户名称'
 
--- produce_order/models.py
+++ produce_order/models.py
@@ -24,22 +24,26 @@ class ProductSize(models.Model):
         verbose_name = u'基础尺寸'
 
 
-class Customer(models.Model):
-    name = models.CharField(u'客户名称', max_length=255)
+class Company(models.Model):
+    name = models.CharField(u'公司名称', max_length=255)
+    address = models.CharField(u'公司地址', max_length=255)
+    person = models.CharField(u'联系人', max_length=25)
+    phone = models.CharField(u'联系电话', max_length=25)
+    fax = models.CharField(u'传真号', max_length=25)
+    kind = models.CharField(u'公司类型', max_length=255)
 
     def __unicode__(self):
         return '%s' % self.name
 
     class Meta:
-        db_table = 'customer'
-        verbose_name = u'客户'
+        db_table = 'company'
+        verbose_name = u'公司'
 
 
-class ProduceOrder(models.Model):
-    order_number = models.CharField(u'生产单编号', max_length=255)
-    name = models.CharField('品名', max_length=255)
-    style = models.CharField('款式', max_length=255)
-    customer = models.ForeignKey(Customer, verbose_name=u'客户')
+class Product(models.Model):
+    name = models.CharField(u'产品名称', max_length=255)
+    style = models.CharField(u'款式', max_length=255)
+    company = models.ForeignKey(Company, verbose_name=u'客户')
     length = models.ForeignKey(ProductSize,
                                related_name='po_by_length',
                                verbose_name=u'长度',
@@ -52,10 +56,32 @@ class ProduceOrder(models.Model):
                                related_name='po_by_height',
                                verbose_name=u'高度',
                                limit_choices_to={'kind': PRODUCT_SIZE_KIND_CHOICES[2][0]})
-    box_number = models.IntegerField(u'纸箱数量')
+
+    def __unicode__(self):
+        return '%s' % self.name
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = u'产品'
+
+
+class ProduceOrder(models.Model):
+    order_number = models.CharField(u'生产单编号', max_length=255)
+    company = models.ForeignKey(Company, verbose_name=u'客户')
+    products = models.ManyToManyField(Product,
+                                      through='OrderProductShip',
+                                      through_fields=('order', 'product'),
+                                      verbose_name=u'产品')
+
     pay_time = models.DateTimeField(u'交货时间')
     create_time = models.DateTimeField(u'创建时间', auto_now_add=True)
 
     class Meta:
         db_table = 'produce_order'
         verbose_name = u'生产单'
+
+
+class OrderProductShip(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(ProduceOrder, on_delete=models.CASCADE)
+    box_number = models.IntegerField(u'纸箱数量')
